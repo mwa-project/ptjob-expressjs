30e397ce930aa9c6443878f4fb01193c274465d2
backslash112 <yangcun@live.com>
2018-06-18 11:42:59 -0500

    remove bodyParser from project

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
 var lessMiddleware = require('less-middleware');
 var logger = require('morgan');
 const cors = require('cors');
@@ -33,7 +32,6 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(bodyParser());
 app.use(lessMiddleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 

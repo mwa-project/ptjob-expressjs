5b2eae8b6968937beb0aff9a5fa81f15d63b5831
backslash112 <yangcun@live.com>
2018-06-19 22:17:48 -0500

    view posts

--- routes/job_posts.js
+++ routes/job_posts.js
@@ -21,6 +21,15 @@ router.get('/:id', function (req, res, next) {
   });
 });
 
+router.get('/user/:id', (req, res, next) => {
+
+  JobPost.find({"created_by.user_id": req.params.id }, (err, posts) => {
+    res.json({
+      "data": posts
+    })
+  });
+});
+
 /* Patch job posts listing. */
 router.patch('/:job_id', function (req, res, next) {
 
@@ -128,28 +137,180 @@ router.delete('/:id', (req, res, next) => {
   });
 });
 
-router.patch('/:id/status/:status', (req, res, next) => {
-  let job_id = req.params.id;
-  let new_status = req.params.status;
+var JobStatus = {
+  Open: "Open",
+  Close: "Close",
+  Taken: "Taken",
+  Completed: "Completed",
+}
+
+var ApplicationStatus = {
+  Submitted: "Submitted",
+  Successful: "Successful",
+  UnSuccessful: "UnSuccessful",
+  Completed: "Completed"
+}
+
+
+let changeJobStatus = (job_id, new_status, callback) => {
   JobPost.findOneAndUpdate(
     { _id: job_id },
     { status: new_status },
     (err, doc) => {
       if (err) return handleError(err);
-      console.log('update Job success')
-      // res.json({ "data": 'success' });
+      console.log('update Job success: -> ' + JobStatus.Completed);
+      callback(true);
     }
   );
-  User.findOneAndUpdate(
-    { "job_applications.job_id": job_id },
-    { "job_applications.$.status": new_status},
-    (err, doc) => {
-      if (err) return handleError(err);
-      console.log('update User success')
-      res.json({ "data": 'success' });
-    }
-  );
+}
+
+// Change the status of job
+let changeJobApplicationsStatus = (job_id, new_status, callback) => {
+  JobPost.findById(job_id, (err, job) => {
+    job.applicants.forEach(app => app.status = new_status);
+    job.save((err, updatedJob) => {
+      console.log(`update status to ${new_status} for ${job.applicants.length} applications`);
+    });
+  })
+}
+// Change the all the applications's status to unsuccess for a job
+let changeJobAllApplicationsToUnSuccess = (job_id, callback) => {
+  changeJobApplicationsStatus(job_id, ApplicationStatus.Successful, callback);
+}
+// Change one application's status to success for a job
+let changeJobOneApplicationToSuccess = (job_id, user_id, callback) => {
+  JobPost.findById(job_id, (err, job) => {
+    job.applicants.forEach(app => {
+      if (app.user_id == user_id) {
+        app.status = ApplicationStatus.Successful;
+      }
+    });
+    job.save((err, updatedJob) => {
+      console.log(`update status to ${new_status} for ${job.applicants.length} applications`);
+    });
+  })
+}
+
+// Change the status of application for user
+let changeUserApplicationStatus = (user_id, job_id, status, callback) => {
+  console.log(`user_id: ${user_id}, job_id: ${job_id}`)
+  User.findById(user_id, (err, user) => {
+    console.log(user);
+    if (!user.job_applications) return callback(true);
+    user.job_applications.forEach(app => {
+      if (app.job_id == job_id) app.status = status;
+    });
+    user.save((err, updatedUser) => {
+      console.log(`update status to ${status} for ${user_id}`);
+    });
+  });
+}
+// Change the application-status to success for one user
+let changeUserApplicationToSuccess = (user_id, job_id, callback) => {
+  changeUserApplicationStatus(user_id, job_id, ApplicationStatus.Successful, callback);
+}
+// Change the application-status to unsuccess for one user
+let changeUserApplicationToUnSuccess = (user_id, job_id, callback) => {
+  changeUserApplicationStatus(user_id, job_id, ApplicationStatus.UnSuccessful, callback);
+}
+
+// Change the worker's status to completed after the job has been marked done 
+let changeUserApplicationToCompleted = (user_id, job_id, callback) => {
+  changeUserApplicationStatus(user_id, job_id, ApplicationStatus.Completed, callback);
+}
+
+router.patch('/:job_id/close', (req, res, next) => {
+  let job_id = req.params.job_id;
+  changeJobStatus(job_id, JobStatus.Close, success => {
+    res.json({ "data": success ? "success" : "failed" })
+  });
+  
 });
+router.patch('/:job_id/competed', (req, res, next) => {
+  let job_id = req.params.job_id;
+  // step 1
+  changeJobStatus(job_id, JobStatus.Completed, success => {
+    // res.json({ "data": success ? "success" : "failed" })
+    console.log(`step 1: change job status ${success}`)
+  });
+
+  // find the worker id
+  JobPost.findById(job_id, (err, job) => {
+    console.log(`step 2: got job: ${job._id}`)
+    let applicants = job.applicants;
+      for (let app of applicants) {
+        if (app.status == ApplicationStatus.Successful) {
+          let user_id = app.user_id;
+          console.log(`step 3: got user: ${user_id}`)
+          // step 2
+          changeUserApplicationToCompleted(user_id, job_id, success => {
+            console.log(`step 4: update user application: ${success}`)
+            res.json({ "data": success ? "success" : "failed" })
+          });
+        }
+      }
+      res.json({ "data": "success" })
+  })
+  
+  
+  
+});
+router.patch('/:job_id/accept/:user_id', (req, res, next) => {
+  try {
+    let job_id = req.params.job_id;
+    console.log(`job id: ${job_id}`)
+    let winner_id = req.params.user_id;
+
+    // step 1
+    changeJobStatus(job_id, JobStatus.Taken, success => {
+      console.log(success)
+    });
+
+    // step 2
+    JobPost.findById(job_id, (err, job) => {
+      console.log(`---> job: ${job}`)
+      let applicants = job.applicants;
+      for (let app of applicants) {
+        changeUserApplicationToUnSuccess(app.user_id, job_id, success => {
+        console.log(success)
+        });
+      }
+    })
+    
+    // step 3
+    changeUserApplicationToSuccess(winner_id, job_id, success => {
+      console.log(success)
+      res.json({ "data": success ? "success" : "failed" })
+    });
+  } catch (error) {
+    console.log(error);
+  }
+  
+  
+});
+
+
+// router.patch('/:job_id/status/:status', (req, res, next) => {
+//   let job_id = req.params.id;
+//   let new_status = req.params.status;
+//   JobPost.findOneAndUpdate(
+//     { _id: job_id },
+//     { status: new_status },
+//     (err, doc) => {
+//       if (err) return handleError(err);
+//       console.log('update Job success')
+//     }
+//   );
+//   User.findOneAndUpdate(
+//     { "job_applications.job_id": job_id },
+//     { "job_applications.$.status": new_status},
+//     (err, doc) => {
+//       if (err) return handleError(err);
+//       console.log('update User success')
+//       res.json({ "data": 'success' });
+//     }
+//   );
+// });
 
 
 module.exports = router;
